ddf2b096252d4928b3d127aff856a343e14bc81d
megy_r <megyr@abv.bg>
2023-03-24 16:31:25 +0200

    Add task
    Math.max_ Math.min _JS

---
+++ asss.js
@@ -0,0 +1,9 @@
+function main(num1, num2, num3, num4, num5)
+{ 
+    top = Math.max(num1,num2,num3, num4, num5);
+    end = Math.min(num1,num2,num3, num4, num5);
+        
+    console.log(top);    
+    console.log(end);    
+}
+main( 2, 100,36 ,8 ,9, 12) 
